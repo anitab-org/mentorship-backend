097320440faee5fac1fae40abb75492d1a6a9d98
Isabel Costa <isabelcmdcosta@gmail.com>
2018-05-23 22:07:32 +0100

    added config file and database test template

---
+++ config.py
@@ -0,0 +1,27 @@
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'EXAMPLE_SECRET_KEY'
+
+    # SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(BaseConfig):
+    #SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///prod_data.db'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev_data.db'
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+    # Use in-memory SQLite database for testing
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test_data.db'
