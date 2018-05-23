7d3feabd15790f0b5869630156610eb217f0d59e
Isabel Costa <isabelcmdcosta@gmail.com>
2018-05-23 23:57:22 +0100

    feat: save pw hash instead of plain text

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,5 +1,6 @@
-from app.database.db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from app.database.db import db
 
 
 class UserModel(db.Model):
@@ -12,12 +13,10 @@ class UserModel(db.Model):
     # personal data
     name = db.Column(db.String(30))
     username = db.Column(db.String(30), unique=True)
-    password = db.Column(db.String(30))
     email = db.Column(db.String(30), unique=True)
 
-    # TODO security, save password as a hash
-
     # security
+    password_hash = db.Column(db.String(100))
     security_question = db.Column(db.String(80))
     security_answer = db.Column(db.String(80))
 
@@ -36,11 +35,13 @@ class UserModel(db.Model):
                  security_question, security_answer, terms_and_conditions_checked):
         self.name = name
         self.username = username
-        self.password = password
         self.email = email
+        self.terms_and_conditions_checked = terms_and_conditions_checked
+
+        # saving hash instead of saving password in plain text
+        self.set_password(password)
         self.security_question = security_question
         self.security_answer = security_answer
-        self.terms_and_conditions_checked = terms_and_conditions_checked
 
         # default values
         self.is_admin = True if self.is_empty() else False  # first user is admin
@@ -52,7 +53,7 @@ class UserModel(db.Model):
             'id': self.id,
             'name': self.name,
             'username': self.username,
-            'password': self.password,
+            'password_hash': self.password_hash,
             'email': self.email,
             'security_question': self.security_question,
             'security_answer': self.security_answer,
@@ -81,6 +82,13 @@ class UserModel(db.Model):
     @classmethod
     def is_empty(cls):
         return cls.query.first() is None
+
+    def set_password(self, password_plain_text):
+        self.password_hash = generate_password_hash(password_plain_text)
+
+    # checks if password is the same, using its hash
+    def check_password(self, password_plain_text):
+        return check_password_hash(self.password_hash, password_plain_text)
 
     def save_to_db(self):
         db.session.add(self)
