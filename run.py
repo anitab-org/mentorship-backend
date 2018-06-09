81b29f3a28e8070ab3d9f15016c73ff83f961c97
Isabel Costa <isabelcmdcosta@gmail.com>
2018-06-09 21:13:33 +0100

    feat: return expiry along with token

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restplus import Api
-from flask_jwt import JWT
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt import JWT
 from app.security import authenticate, identity
+from datetime import datetime
 
 CONFIG_NAME_MAPPER = {
     'development': 'config.DevelopmentConfig',
@@ -21,10 +22,6 @@ if flask_config_name is None:
 
 application.config.from_object(CONFIG_NAME_MAPPER[flask_config_name])
 
-db = SQLAlchemy(application)
-# returns 'access_token'
-jwt = JWT(application, authenticate, identity)
-
 api = Api(
     app=application,
     title='Mentorship System API',
@@ -34,13 +31,6 @@ api = Api(
 )
 
 
-@application.before_first_request
-def create_tables():
-    from app.database.db_utils import db
-    # db.drop_all()
-    db.create_all()
-
-
 # Adding namespaces
 def add_namespaces():
     # called here to avoid circular imports
@@ -48,6 +38,33 @@ def add_namespaces():
     from app.api.resources.admin import admin_ns as admin_namespace
     api.add_namespace(user_namespace, path='/')
     api.add_namespace(admin_namespace, path='/')
+
+
+jwt = JWT(application, authenticate, identity)
+
+
+@jwt.auth_response_handler
+def custom_jwt_response_handler(access_token, identity):
+    payload = jwt.jwt_payload_callback(identity)
+
+    if 'exp' in payload:
+        expiry = payload['exp']
+    else:
+        expiry = datetime.utcnow() + application.config.get('JWT_EXPIRATION_DELTA')
+
+    return jsonify({
+        'access_token': access_token.decode('utf-8'),
+        'expiry': expiry.timestamp()
+    })
+
+
+db = SQLAlchemy(application)
+
+
+@application.before_first_request
+def create_tables():
+    # db.drop_all()
+    db.create_all()
 
 
 if __name__ == "__main__":
