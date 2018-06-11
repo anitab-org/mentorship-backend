bd46c852530ee1caf54a937187eeced1c9fccf97
Isabel Costa <isabelcmdcosta@gmail.com>
2018-06-11 19:45:11 +0100

    feat: validate email regex

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -3,7 +3,7 @@ from flask_testing import TestCase
 from app.database.models.user import UserModel
 from run import application, db
 
-from tests.utils import test_admin_user
+from tests.test_data import test_admin_user
 
 
 class BaseTestCase(TestCase):
