8f2f4d60dd9ffee8f3d8788ce7972063e1734eef
Isabel Costa <isabelcmdcosta@gmail.com>
2018-06-23 21:58:37 +0100

    feat: revoke admin status

--- app/api/resources/admin.py
+++ app/api/resources/admin.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restplus import Resource
 from flask_jwt import jwt_required, current_identity
+
 from run import api
 from app.api.models.admin import *
 from app.api.dao.admin import AdminDAO
@@ -17,7 +18,7 @@ class AssignNewUserAdmin(Resource):
 
     @classmethod
     @jwt_required()
-    @admin_ns.expect(auth_header_parser, assign_new_admin_request_body, validate=True)
+    @admin_ns.expect(auth_header_parser, assign_and_revoke_user_admin_request_body, validate=True)
     def post(cls):
         """
         Assigns a User as a new Admin.
@@ -25,9 +26,30 @@ class AssignNewUserAdmin(Resource):
 
         if current_identity.is_admin:
             data = request.json
-            return DAO.assign_new_user(data)
+            return DAO.assign_new_user(current_identity.id, data)
 
         else:
             return {
-                       "message": "You don't have admin status. You can't assign another admin"
-                   }, 401
+                       "message": "You don't have admin status. You can't assign other user as admin."
+                   }, 403
+
+
+@admin_ns.route('admin/remove')
+class RevokeUserAdmin(Resource):
+
+    @classmethod
+    @jwt_required()
+    @admin_ns.expect(auth_header_parser, assign_and_revoke_user_admin_request_body, validate=True)
+    def post(cls):
+        """
+        Revoke admin status from another User Admin.
+        """
+
+        if current_identity.is_admin:
+            data = request.json
+            return DAO.revoke_admin_user(current_identity.id, data)
+
+        else:
+            return {
+                       "message": "You don't have admin status. You can't revoke other admin user."
+                   }, 403
