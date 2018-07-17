b3900e60fb7cad0fe98f750ceb5d507e3cd18004
Isabel Costa <isabelcmdcosta@gmail.com>
2018-07-17 12:27:20 +0100

    feat: add organization to user profile (#90)

---
+++ tests/users/test_dao_update_user.py
@@ -0,0 +1,25 @@
+import unittest
+
+from app.api.dao.user import UserDAO
+from tests.base_test_case import BaseTestCase
+
+
+class TestUpdateUserDao(BaseTestCase):
+
+    def test_dao_update_user(self):
+
+        self.assertIsNone(self.admin_user.occupation)
+        self.assertIsNone(self.admin_user.organization)
+
+        data = dict(
+            occupation='good_developer',
+            organization='good_org',
+        )
+        UserDAO.update_user_profile(self.admin_user.id, data)
+
+        self.assertEqual('good_developer', self.admin_user.occupation)
+        self.assertEqual('good_org', self.admin_user.organization)
+
+
+if __name__ == '__main__':
+    unittest.main()
