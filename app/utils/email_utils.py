74cf7a654f32af17031c59bdc510d3245e222b1e
Isabel Costa <isabelcmdcosta@gmail.com>
2018-07-19 11:08:42 +0100

    Revert "Fixed User registration request data validation"

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -1,12 +1,7 @@
 import re
 
 email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-username_regex = r"(^[a-zA-Z0-9]+$)"
 
 
 def is_email_valid(email):
     return re.match(email_regex, email)
-
-
-def is_username_valid(username):
-    return re.match(username_regex, username)
