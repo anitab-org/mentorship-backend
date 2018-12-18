d0adeab2389fb8acd8849f08de3db6e2ae7c6c4d
Isfaaq <isfaaqg@gmail.com>
2018-12-18 12:16:07 +0400

    style: Add doc strings for UserDao class and its functions

--- app/api/dao/user.py
+++ app/api/dao/user.py
@@ -6,12 +6,43 @@ from app.utils.validation_utils import is_email_valid
 
 
 class UserDAO:
+    """ Class definition for UserDAO.
+    
+    Provides various functions pertaining to user.
+
+    Attributes:
+        FAIL_USER_ALREADY_EXISTS
+        SUCCESS_USER_CREATED
+        MIN_NUMBER_OF_ADMINS
+
+    Functions:
+        create_user()
+        delete_user()
+        get_user()
+        get_user_by_email()
+        get_user_by_username()
+        list_users()
+        update_user_profile()
+        change_password()
+        confirm_registration()
+        authenticate()
+    """ 
     FAIL_USER_ALREADY_EXISTS = "FAIL_USER_ALREADY_EXISTS"
     SUCCESS_USER_CREATED = "SUCCESS_USER_CREATED"
     MIN_NUMBER_OF_ADMINS = 1
 
     @staticmethod
     def create_user(data):
+        """Creates a user.
+
+        Creates a new user if the current username is not taken. Otherwise returns a message.
+
+        Args:
+            data: A list containing the name, username, password, email and terms_and_conditions_checked.
+
+        Returns:
+            message: A message corresponding to the completed action; success or failure.
+        """   
         name = data['name']
         username = data['username']
         password = data['password']
@@ -41,6 +72,17 @@ class UserDAO:
 
     @staticmethod
     def delete_user(user_id):
+        """Deletes a user.
+
+        Deletes a user provided the latter exists and deleting them will not cause the number of admins to go below the minimum threshold.
+
+        Args:
+            user_id : The user id of the user being deleted.
+
+        Returns:
+            message: A message containg a description of the action performed.
+        """
+
         user = UserModel.find_by_id(user_id)
 
         # check if this user is the only admin
@@ -58,18 +100,63 @@ class UserDAO:
 
     @staticmethod
     def get_user(user_id):
+        """Finds a user.
+
+        Returns a user which has been searched by their id.
+
+        Args:
+            user_id : The user id of the required user.
+
+        Returns:
+            user: The corresponding user.
+        """
+
         return UserModel.find_by_id(user_id)
 
     @staticmethod
     def get_user_by_email(email):
+        """Finds a user by email.
+
+        Returns a user which has been searched by their email.
+
+        Args:
+            email : The email of the required user.
+
+        Returns:
+            user: The corresponding user.
+        """
+
         return UserModel.find_by_email(email)
 
     @staticmethod
     def get_user_by_username(username):
+        """Finds a user by username.
+
+        Returns a user which has been searched by their username.
+
+        Args:
+            username : The username of the required user.
+
+        Returns:
+            user: The corresponding user.
+        """
+
         return UserModel.find_by_username(username)
 
     @staticmethod
     def list_users(user_id, is_verified=None):
+        """Lists users.
+
+        Returns a list of users which has been searched by their user_id.
+
+        Args:
+            list_of_users : List of user ids.
+            is_verified: whether or not the results should contain only verified users.
+
+        Returns:
+            users_list: A list of users matching the given criteria.
+        """
+
         users_list = UserModel.query.filter(UserModel.id!=user_id).all()
         list_of_users = []
         if is_verified:
@@ -83,6 +170,17 @@ class UserDAO:
 
     @staticmethod
     def update_user_profile(user_id, data):
+        """Updates a user's profile.
+
+        Updates the information pertaining to a given user and saves it to the database.
+
+        Args:
+            user_id : user_id of the user whose details are to be updated.
+            data: List containing the fields to be updated and their corresponding values.
+
+        Returns:
+            message: A message corresponding to the performed action.
+        """
 
         user = UserModel.find_by_id(user_id)
         if not user:
@@ -143,6 +241,18 @@ class UserDAO:
 
     @staticmethod
     def change_password(user_id, data):
+        """Change the password of a user.
+
+        Changes the password of a given user and saves it to the database.
+
+        Args:
+            user_id : user_id of the user whose details are to be updated.
+            data: List containing the current and new password and their corresponding values.
+
+        Returns:
+            message: A message corresponding to the performed action.
+        """
+
         current_password = data['current_password']
         new_password = data['new_password']
 
@@ -156,6 +266,16 @@ class UserDAO:
 
     @staticmethod
     def confirm_registration(token):
+        """Confirms a newly registered user.
+
+        Sets the is_email_verified of a user to True if it is not already the case and saves to the database.
+
+        Args:
+            token : Token which has been emailed.
+
+        Returns:
+            message: A message corresponding to the performed action.
+        """
 
         email_from_token = confirm_token(token)
 
@@ -173,10 +293,16 @@ class UserDAO:
 
     @staticmethod
     def authenticate(username_or_email, password):
-        """
+        """Authenticates a user.
+
+        Authenticates a user given the latter has a valid email and provides the appropriate password.
         The user can login with two options:
-        -> username + password
-        -> email + password
+            -> username + password
+            -> email + password
+
+        Args:
+            username_or_email : username or email of the user being authenticated.
+            password: List containing the current and new password and their corresponding values.
         """
 
         if is_email_valid(username_or_email):
