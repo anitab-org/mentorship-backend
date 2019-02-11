720385296ea075a04a8ea51d2768f49dd39f515a
LordGameleo <f2016681@pilani.bits-pilani.ac.in>
2019-02-11 21:07:32 +0530

    Changes on naming and including args and kwargs

--- app/utils/decorator_utils.py
+++ app/utils/decorator_utils.py
@@ -3,10 +3,17 @@ from app.database.models.user import UserModel
 
 def check_mail_confirmation(func_to_validate):
 
-    def to_validate_user(user_id, data):
-        user = UserModel.find_by_id(user_id)
-        if user.is_email_verified:
-            return func_to_validate(user_id, data)
+    def to_validate_user(*args, **kwargs):
+
+        if len(kwargs) != 0:
+            user = UserModel.find_by_id(kwargs['user_id'])
         else:
-            return {"message": "You have not confirmed your email."}, 400
+            user = UserModel.find_by_id(args[0])
+
+        if user.is_email_verified:
+            return func_to_validate(*args, **kwargs)
+        else:
+            return func_to_validate(*args, **kwargs)
+            
+            return {'message' : 'You have not confirmed your email.'}, 200
     return to_validate_user
