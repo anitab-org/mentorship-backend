a623c610dccced39b111fae0e7f93818d2dd35b4
LordGameleo <f2016681@pilani.bits-pilani.ac.in>
2019-02-12 23:38:15 +0530

    Made decorator_utils.py readable

--- app/utils/decorator_utils.py
+++ app/utils/decorator_utils.py
@@ -1,19 +1,32 @@
 from app.database.models.user import UserModel
 
 
+# definition of decorator to validate user
 def user_validation(user_function):
 
     def validator(*args, **kwargs):
 
+        #  check if the given paramenters are in form of dict (to get user_id)
         if len(kwargs) != 0:
+
+            # if parameters are given in form of dict
             user = UserModel.find_by_id(kwargs['user_id'])
+
         else:
+
+            # if parameters are given as args
             user = UserModel.find_by_id(args[0])
+
+        # verify if user exists
         if user:
+
+            # verify if user has verified his/her email
             if user.is_email_verified:
                 return user_function(*args, **kwargs)
+
             else:
                 return {'message': 'You have not confirmed your email.'}, 400
+
         else:
             return {'message': 'User does not exist.'}, 404
 
