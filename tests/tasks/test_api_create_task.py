b239b6dd7e6a5f93f51b53a180158de032dc5b65
Hardik0403 <58501114+Hardik0403@users.noreply.github.com>
2019-12-23 17:37:32 +0530

    feat: Added more tests to the create task api.

--- tests/tasks/test_api_create_task.py
+++ tests/tasks/test_api_create_task.py
@@ -5,6 +5,8 @@ from app import messages
 from tests.tasks.tasks_base_setup import TasksBaseTestCase
 from tests.test_utils import get_test_request_header
 
+from datetime import timedelta
+
 
 class TestCreateTaskApi(TasksBaseTestCase):
 
@@ -35,6 +37,44 @@ class TestCreateTaskApi(TasksBaseTestCase):
         self.assertEqual(self.test_description, new_task.get('description'))
         self.assertEqual(self.test_is_done, new_task.get('is_done'))
 
+# This case tests whether the user is in a mentorship relation or not while creating a new task.
+# If the user is not in a mentorship relation and he tries to create a task, then
+# '404:Not found' error would be given displaying the message that 'Mentorship relation does not exist'.
+    def test_create_task_api_user_not_in_mentorship_relation(self):
+        auth_header=get_test_request_header(self.first_user.id)
+        expected_response=messages.MENTORSHIP_RELATION_DOES_NOT_EXIST
+        actual_response = self.client.post('/mentorship_relation/%s/task' % 123123,
+                                            follow_redirects=True, headers=auth_header, content_type='application/json',
+                                            data=json.dumps(dict(description=self.test_description)))
+
+        self.assertEqual(404,actual_response.status_code)
+        self.assertDictEqual(expected_response,json.loads(actual_response.data))
+
+# This case tests the creation of task without description field.
+# If a user creates a task without a description field, then
+# '400:Bad Request' error would be given displaying the message that 'Description field is missing'.
+    def test_create_task_api_empty_description_field(self):
+        auth_header=get_test_request_header(self.first_user.id)
+        expected_response=messages.DESCRIPTION_FIELD_IS_MISSING
+        actual_response=self.client.post('/mentorship_relation/%s/task' % self.mentorship_relation_w_second_user.id,
+                                         follow_redirects=True, headers=auth_header, content_type='application/json',
+                                         data=json.dumps(dict()))
+
+        self.assertEqual(400,actual_response.status_code)
+        self.assertDictEqual(expected_response,json.loads(actual_response.data))
+
+# This case tests the creation of task with an expired authentication token.
+# If a user creates a task with an expired authentication token, then
+# '401:Unauthorised' error would be given displaying the message that 'The token has expired! Please, login again or refresh it.'.
+    def test_create_task_api_with_token_expired(self):
+        auth_header=get_test_request_header(self.first_user.id,token_expiration_delta=timedelta(minutes=-5))
+        expected_response=messages.TOKEN_HAS_EXPIRED
+        actual_response = self.client.post('/mentorship_relation/%s/task' % self.mentorship_relation_w_second_user.id,
+                                           follow_redirects=True, headers=auth_header, content_type='application/json',
+                                           data=json.dumps(dict(description=self.test_description)))
+
+        self.assertEqual(401, actual_response.status_code)
+        self.assertDictEqual(expected_response, json.loads(actual_response.data))
 
 if __name__ == "__main__":
     unittest.main()
