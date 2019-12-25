032096b3853f7eb022c1553b3efe6ff2e1fffa29
Bartek Pacia <barpac02@gmail.com>
2019-12-25 19:23:48 +0100

    applied mentor's suggestion: test asserts USER_IS_INVOLVED_IN_A_MENTORSHIP_RELATION

--- tests/mentorship_relation/test_api_accept_request.py
+++ tests/mentorship_relation/test_api_accept_request.py
@@ -49,12 +49,27 @@ class TestAcceptMentorshipRequestApi(MentorshipRelationBaseTestCase):
 
     def test_accept_mentorship_request_user_already_in_relation(self):
         self.assertEqual(MentorshipRelationState.PENDING, self.mentorship_relation.state)
+
+        self.mentorship_relation_2 = MentorshipRelationModel(
+            action_user_id=self.second_user.id,
+            mentor_user=self.admin_user,
+            mentee_user=self.second_user,
+            creation_date=self.now_datetime.timestamp(),
+            end_date=self.end_date_example.timestamp(),
+            state=MentorshipRelationState.PENDING,
+            notes=self.notes_example,
+            tasks_list=TasksListModel()
+        )
+
+        db.session.add(self.mentorship_relation_2)
+        db.session.commit()
+
         with self.client:
-            response = self.client.put(f"/mentorship_relation/{self.mentorship_relation.id}/accept",
-                                       headers=get_test_request_header(self.second_user.id))
+            response = self.client.put(f"/mentorship_relation/{self.mentorship_relation_2.id}/accept",
+                                       headers=get_test_request_header(self.admin_user.id))
 
             self.assertEqual(200, response.status_code)
-            self.assertEqual(MentorshipRelationState.ACCEPTED, self.mentorship_relation.state)
+            self.assertEqual(MentorshipRelationState.ACCEPTED, self.mentorship_relation_2.state)
             self.assertDictEqual(messages.MENTORSHIP_RELATION_WAS_ACCEPTED_SUCCESSFULLY,
                                  json.loads(response.data))
 
@@ -63,8 +78,8 @@ class TestAcceptMentorshipRequestApi(MentorshipRelationBaseTestCase):
                                        headers=get_test_request_header(self.second_user.id))
 
             self.assertEqual(400, response.status_code)
-            self.assertEqual(MentorshipRelationState.ACCEPTED, self.mentorship_relation.state)
-            self.assertDictEqual(messages.NOT_PENDING_STATE_RELATION,
+            self.assertEqual(MentorshipRelationState.PENDING, self.mentorship_relation.state)
+            self.assertDictEqual(messages.USER_IS_INVOLVED_IN_A_MENTORSHIP_RELATION,
                                  json.loads(response.data))
 
     def test_accept_not_valid_user(self):
