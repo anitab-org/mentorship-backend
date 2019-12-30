55680b35bd2f89d25226389ac9714fd00c48315f
asawesome07 <abhinav.wii@gmail.com>
2019-12-30 18:57:18 +0530

    added task comment model and dao

--- app/messages.py
+++ app/messages.py
@@ -168,6 +168,10 @@ MENTORSHIP_RELATION_WAS_REJECTED_SUCCESSFULLY = {"message": "Mentorship"
 MENTORSHIP_RELATION_WAS_CANCELLED_SUCCESSFULLY = {"message": "Mentorship"
                                                   " relation was "
                                                   "cancelled successfully"}
+TASK_COMMENT_WAS_CREATED_SUCCESSFULLY = {'message': "Task comment was created successfully."}
+TASK_COMMENT_WAS_DELETED_SUCCESSFULLY = {"message": "Task comment was deleted successfully"}
+TASK_COMMENT_WAS_MODIFIED_SUCCESSFULLY = {'message': "Task comment was modified successfully"}
+TASK_COMMENT_DOES_NOT_EXIST = {'message': "Task comment with given id does not exist"}
 TASK_WAS_CREATED_SUCCESSFULLY = {"message": "Task was created successfully."}
 TASK_WAS_DELETED_SUCCESSFULLY = {"message": "Task was deleted successfully."}
 TASK_WAS_ACHIEVED_SUCCESSFULLY = {"message": "Task was achieved"
