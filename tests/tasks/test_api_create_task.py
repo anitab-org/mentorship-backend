a0da7c6022922e71d80bfbb1d11c07980d38f0ed
Ilya Maier <ilya.maier@outlook.com>
2020-01-31 21:56:56 +0100

    chore: Refactor tests/tasks folder

--- tests/tasks/test_api_create_task.py
+++ tests/tasks/test_api_create_task.py
@@ -10,25 +10,31 @@ class TestCreateTaskApi(TasksBaseTestCase):
 
     def test_create_task_api_resource_non_auth(self):
         expected_response = messages.AUTHORISATION_TOKEN_IS_MISSING
-        actual_response = self.client.post('/mentorship_relation/%s/task' % self.mentorship_relation_w_second_user.id,
-                                           follow_redirects=True)
+        actual_response = self.client.post(
+            '/mentorship_relation/%s/task' %
+            self.mentorship_relation_w_second_user.id,
+            follow_redirects=True)
 
         self.assertEqual(401, actual_response.status_code)
-        self.assertDictEqual(expected_response, json.loads(actual_response.data))
+        self.assertDictEqual(expected_response,
+                             json.loads(actual_response.data))
 
     def test_full_task_creation_api(self):
-
         non_existent_task = self.tasks_list_1.find_task_by_id(3)
         self.assertIsNone(non_existent_task)
 
         auth_header = get_test_request_header(self.first_user.id)
         expected_response = messages.TASK_WAS_CREATED_SUCCESSFULLY
-        actual_response = self.client.post('/mentorship_relation/%s/task' % self.mentorship_relation_w_second_user.id,
-                                           follow_redirects=True, headers=auth_header, content_type='application/json',
-                                           data=json.dumps(dict(description=self.test_description)))
+        actual_response = self.client.post(
+            '/mentorship_relation/%s/task' %
+            self.mentorship_relation_w_second_user.id,
+            follow_redirects=True, headers=auth_header,
+            content_type='application/json',
+            data=json.dumps(dict(description=self.test_description)))
 
         self.assertEqual(200, actual_response.status_code)
-        self.assertDictEqual(expected_response, json.loads(actual_response.data))
+        self.assertDictEqual(expected_response,
+                             json.loads(actual_response.data))
 
         new_task = self.tasks_list_1.find_task_by_id(3)
         self.assertIsNotNone(new_task)
