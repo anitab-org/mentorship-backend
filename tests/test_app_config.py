6c1ffcda704bd12650126beb5d75de929e67bd0d
Ilya Maier <ilya.maier@outlook.com>
2020-01-31 22:16:10 +0100

    chore: Refactor tests folder

--- tests/test_app_config.py
+++ tests/test_app_config.py
@@ -12,7 +12,8 @@ class TestTestingConfig(TestCase):
         application.config.from_object('config.TestingConfig')
 
         secret_key = os.getenv('SECRET_KEY', None)
-        application.config['SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
+        application.config[
+            'SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
         return application
 
     def test_app_testing_config(self):
@@ -20,12 +21,15 @@ class TestTestingConfig(TestCase):
         self.assertTrue(application.config['DEBUG'])
         self.assertTrue(application.config['TESTING'])
         self.assertFalse(application.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
-        self.assertEqual('sqlite://', application.config['SQLALCHEMY_DATABASE_URI'])
+        self.assertEqual('sqlite://',
+                         application.config['SQLALCHEMY_DATABASE_URI'])
         self.assertIsNotNone(current_app)
 
         # testing JWT configurations
-        self.assertEqual(timedelta(weeks=1), application.config['JWT_ACCESS_TOKEN_EXPIRES'])
-        self.assertEqual(timedelta(weeks=4), application.config['JWT_REFRESH_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=1),
+                         application.config['JWT_ACCESS_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=4),
+                         application.config['JWT_REFRESH_TOKEN_EXPIRES'])
 
 
 class TestDevelopmentConfig(TestCase):
@@ -33,7 +37,8 @@ class TestDevelopmentConfig(TestCase):
         application.config.from_object('config.DevelopmentConfig')
 
         secret_key = os.getenv('SECRET_KEY', None)
-        application.config['SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
+        application.config[
+            'SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
         return application
 
     def test_app_development_config(self):
@@ -41,12 +46,15 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(application.config['DEBUG'])
         self.assertFalse(application.config['TESTING'])
         self.assertFalse(application.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
-        self.assertEqual('sqlite:///dev_data.db', application.config['SQLALCHEMY_DATABASE_URI'])
+        self.assertEqual('sqlite:///dev_data.db',
+                         application.config['SQLALCHEMY_DATABASE_URI'])
         self.assertIsNotNone(current_app)
 
         # testing JWT configurations
-        self.assertEqual(timedelta(weeks=1), application.config['JWT_ACCESS_TOKEN_EXPIRES'])
-        self.assertEqual(timedelta(weeks=4), application.config['JWT_REFRESH_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=1),
+                         application.config['JWT_ACCESS_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=4),
+                         application.config['JWT_REFRESH_TOKEN_EXPIRES'])
 
 
 class TestProductionConfig(TestCase):
@@ -54,7 +62,8 @@ class TestProductionConfig(TestCase):
         application.config.from_object('config.ProductionConfig')
 
         secret_key = os.getenv('SECRET_KEY', None)
-        application.config['SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
+        application.config[
+            'SECRET_KEY'] = secret_key if secret_key else 'TEST_SECRET_KEY'
         return application
 
     def test_app_production_config(self):
@@ -62,12 +71,15 @@ class TestProductionConfig(TestCase):
         self.assertFalse(application.config['DEBUG'])
         self.assertFalse(application.config['TESTING'])
         self.assertFalse(application.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
-        self.assertEqual('sqlite:///prod_data.db', application.config['SQLALCHEMY_DATABASE_URI'])
+        self.assertEqual('sqlite:///prod_data.db',
+                         application.config['SQLALCHEMY_DATABASE_URI'])
         self.assertIsNotNone(current_app)
 
         # testing JWT configurations
-        self.assertEqual(timedelta(weeks=1), application.config['JWT_ACCESS_TOKEN_EXPIRES'])
-        self.assertEqual(timedelta(weeks=4), application.config['JWT_REFRESH_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=1),
+                         application.config['JWT_ACCESS_TOKEN_EXPIRES'])
+        self.assertEqual(timedelta(weeks=4),
+                         application.config['JWT_REFRESH_TOKEN_EXPIRES'])
 
 
 if __name__ == '__main__':
