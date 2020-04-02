e750552e5e6b0df3859602fde0f46e08aa572a7d
EfthymiaKostaki <kostakiefthymia@gmail.com>
2020-04-02 12:51:01 +0300

    refactor: add description of the app

--- app/api/api_extension.py
+++ app/api/api_extension.py
@@ -3,7 +3,9 @@ from flask_restplus import Api
 api = Api(
     title="Mentorship System API",
     version="1.0",
-    description="API documentation for the backend of Mentorship System \n \n"
+    description="API documentation for the backend of Mentorship System. \n \n"
+    + "Mentorship System is an application that matches women in tech to mentor each other, on career development, "
+    + "through 1:1 relations during a certain period of time. \n \n"
     + "The main repository of the Backend System can be found here: https://github.com/anitab-org/mentorship-backend \n \n"
     + "The Android client for the Mentorship System can be found here: https://github.com/anitab-org/mentorship-android \n \n"
     + "For more information about the project here's a link to our wiki guide: https://github.com/anitab-org/mentorship-backend/wiki"
