57013a347a951840ce601a5cb062335a796d42a4
vj-codes <vj.codes29@gmail.com>
2020-07-18 20:02:38 +0530

    changed http code in tests part3

--- tests/mentorship_relation/test_dao_reject_mentorship_request.py
+++ tests/mentorship_relation/test_dao_reject_mentorship_request.py
@@ -106,11 +106,11 @@ class TestMentorshipRelationListingDAO(MentorshipRelationBaseTestCase):
         db.session.commit()
 
         result = DAO.reject_request(self.second_user.id, self.mentorship_relation.id)
-        self.assertEqual((messages.NOT_PENDING_STATE_RELATION, 400), result)
+        self.assertEqual((messages.NOT_PENDING_STATE_RELATION, 403), result)
 
         self.mentorship_relation.state = MentorshipRelationState.REJECTED
         db.session.add(self.mentorship_relation)
         db.session.commit()
 
         result = DAO.reject_request(self.second_user.id, self.mentorship_relation.id)
-        self.assertEqual((messages.NOT_PENDING_STATE_RELATION, 400), result)
+        self.assertEqual((messages.NOT_PENDING_STATE_RELATION, 403), result)
