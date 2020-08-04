ad6fd3ac325a3a0e48dac84814712b2ed2e5a81a
Isabel Costa <isabelcmdcosta@gmail.com>
2020-08-04 20:54:52 +0100

    style: ran black on file

--- tests/user_journey/happy_path_1.py
+++ tests/user_journey/happy_path_1.py
@@ -29,7 +29,6 @@ class TestHappyPath1(BaseTestCase):
     - User B (mentee) completes the task
     """
 
-
     def setUp(self):
         super(TestHappyPath1, self).setUp()
 
@@ -39,7 +38,7 @@ class TestHappyPath1(BaseTestCase):
             username=user1["username"],
             password=user1["password"],
             terms_and_conditions_checked=user1["terms_and_conditions_checked"],
-        ) # User A
+        )  # User A
 
         self.mentee = UserModel(
             name="Mentee B",
@@ -47,7 +46,7 @@ class TestHappyPath1(BaseTestCase):
             username=user2["username"],
             password=user2["password"],
             terms_and_conditions_checked=user2["terms_and_conditions_checked"],
-        ) # User B
+        )  # User B
 
         self.mentor.is_email_verified = True
         self.mentee.is_email_verified = True
@@ -66,23 +65,27 @@ class TestHappyPath1(BaseTestCase):
 
         # - User B (mentee) sends a request to User A (mentor)
         # POST /mentorship_relation/send_request
-        
-        request_body={
+
+        request_body = {
             "mentor_id": self.mentor.id,
             "mentee_id": self.mentee.id,
-            "end_date": int((datetime.now()+timedelta(days=40)).timestamp()),
-            "notes": "some notes"
+            "end_date": int((datetime.now() + timedelta(days=40)).timestamp()),
+            "notes": "some notes",
         }
-        send_request_response = self.client.post('/mentorship_relation/send_request',
-                                           headers=mentee_auth_header,
-                                           content_type='application/json',
-                                           data=json.dumps(request_body))
+        send_request_response = self.client.post(
+            "/mentorship_relation/send_request",
+            headers=mentee_auth_header,
+            content_type="application/json",
+            data=json.dumps(request_body),
+        )
 
         self.assertEqual(201, send_request_response.status_code)
 
-        request_sent_response = self.client.get('/mentorship_relations',
-                                           headers=mentor_auth_header,
-                                           content_type='application/json')
+        request_sent_response = self.client.get(
+            "/mentorship_relations",
+            headers=mentor_auth_header,
+            content_type="application/json",
+        )
 
         requests = json.loads(request_sent_response.data)
         mentorship_request = requests[0]
@@ -93,21 +96,27 @@ class TestHappyPath1(BaseTestCase):
         self.assertEqual(MentorshipRelationState.PENDING, request_state)
 
         # - User A (mentor) accepts request
-        # POST /mentorship_relation​/{request_id}​/accept
+        # POST /mentorship_relation/{request_id}/accept
 
-        accept_response = self.client.put(f"/mentorship_relation/{request_id}/accept",
-                                           headers=mentor_auth_header)
+        accept_response = self.client.put(
+            f"/mentorship_relation/{request_id}/accept", headers=mentor_auth_header
+        )
 
         self.assertEqual(200, accept_response.status_code)
 
-        mentee_current_relation = self.client.get(f"/mentorship_relations/current",
-                                           headers=mentor_auth_header)
-        mentor_current_relation = self.client.get(f"/mentorship_relations/current",
-                                           headers=mentor_auth_header)
+        mentee_current_relation = self.client.get(
+            f"/mentorship_relations/current", headers=mentor_auth_header
+        )
+        mentor_current_relation = self.client.get(
+            f"/mentorship_relations/current", headers=mentor_auth_header
+        )
 
         self.assertEqual(200, mentee_current_relation.status_code)
         self.assertEqual(200, mentor_current_relation.status_code)
-        self.assertEqual(json.loads(mentor_current_relation.data), json.loads(mentee_current_relation.data))
+        self.assertEqual(
+            json.loads(mentor_current_relation.data),
+            json.loads(mentee_current_relation.data),
+        )
 
         current_relation = json.loads(mentee_current_relation.data)
         request_id = current_relation["id"]
