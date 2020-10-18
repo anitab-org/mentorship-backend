57fa18d88fef9893ab971f9a1accd1c30781d70a
Bartek Pacia <barpac02@gmail.com>
2020-10-18 21:44:58 +0200

    feat: use Poetry for package management

---
+++ scripts.py
@@ -0,0 +1,11 @@
+# This is a temporary workaround till Poetry supports scripts, see
+# https://github.com/sdispater/poetry/issues/241.
+from subprocess import check_call
+
+
+def start():
+  check_call(["python", "run.py"])
+
+
+def test():
+  check_call(["python", "-m", "unittest", "discover", "tests"])
