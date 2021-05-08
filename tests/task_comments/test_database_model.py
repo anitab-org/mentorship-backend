1a562ff794b2b74a7a30f43748517860707d9791
epicadk <adityakurkure@gmail.com>
2021-05-08 22:59:48 +0530

    fix formatting

--- tests/task_comments/test_database_model.py
+++ tests/task_comments/test_database_model.py
@@ -10,13 +10,11 @@ class TestAdminUserModel(BaseTestCase):
             user_id=1,
             task_id=1,
             relation_id=1,
-            comment=""
+            comment="",
         )
 
     def test_task_validations_comment_strip(self):
         comment = TaskCommentModel(
-            user_id=1,
-            task_id=1,
-            relation_id=1,
-            comment=" user ")
+            user_id=1, task_id=1, relation_id=1, comment=" user "
+        )
         self.assertEqual(comment.comment, "user")
