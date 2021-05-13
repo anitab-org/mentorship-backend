458bcc0053bbf5435b71dbdb8d9a1d9660f7db5d
B.Ashita <battua@iitbhilai.ac.in>
2021-05-14 01:13:51 +0530

    made changes

--- tests/users/test_dao_filter_by_skill.py
+++ tests/users/test_dao_filter_by_skill.py
@@ -1,6 +1,7 @@
 import unittest
 
 from flask import json
+from http import HTTPStatus
 from tests.base_test_case import BaseTestCase
 from app.database.models.user import UserModel
 from app.database.sqlalchemy_extension import db
@@ -8,7 +9,7 @@ from tests.test_data import user1, user2, user3
 from tests.test_utils import get_test_request_header
 
 
-class FilterUsersBySkill(BaseTestCase):
+class TestFilterUsersBySkill(BaseTestCase):
     def insert_entries_in_database(self):
 
         # Insert data of the first entry
@@ -53,35 +54,43 @@ class FilterUsersBySkill(BaseTestCase):
         db.session.add(self.third_user)
         db.session.commit()
 
-    def test_case_1(self):
+    def test_filter_users_by_skill_problem_solving(self):
         self.insert_entries_in_database()
 
         auth_header = get_test_request_header(self.admin_user.id)
-        expected_response_1 = "Problem Solving"
-
-        actual_response_1 = self.client.get(
+        expected_response = {"user1": "Problem Solving", "user2": "Problem Solving"}
+        
+        actual_response = self.client.get(
             "/users/verified?skills=Problem Solving",
             headers=auth_header,
             content_type="application/json",
         )
 
-        for data in json.loads(actual_response_1.data):
-            self.assertEqual(expected_response_1, data["skills"])
+        self.assertEqual(HTTPStatus.OK, actual_response.status_code)
 
-    def test_case_2(self):
+        for data in json.loads(actual_response.data):
+
+            if data["username"] in expected_response.keys():
+                self.assertEqual(expected_response[data["username"]], data["skills"])
+
+    def test_filter_users_by_skill_critical_thinking(self):
         self.insert_entries_in_database()
 
         auth_header = get_test_request_header(self.admin_user.id)
-        expected_response_2 = "Critical thinking"
+        expected_response = {"user3": "Critical thinking"}
 
-        actual_response_2 = self.client.get(
+        actual_response = self.client.get(
             "/users/verified?skills=Critical thinking",
             headers=auth_header,
             content_type="application/json",
         )
 
-        for data in json.loads(actual_response_2.data):
-            self.assertEqual(expected_response_2, data["skills"])
+        self.assertEqual(HTTPStatus.OK, actual_response.status_code)
+
+        for data in json.loads(actual_response.data):
+
+            if data["username"] in expected_response.keys():
+                self.assertEqual(expected_response[data["username"]], data["skills"])
 
 
 if __name__ == "__main__":
