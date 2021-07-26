f7bc61a57cd88b83e491aad0e71f177b894f91c3
Aditya Kurkure <56596662+epicadk@users.noreply.github.com>
2021-07-26 11:36:03 +0530

    Update user.py

--- app/api/resources/user.py
+++ app/api/resources/user.py
@@ -5,7 +5,6 @@ from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
     get_jwt_identity,
-    jwt_refresh_token_required,
     jwt_required,
 )
 from flask_restx import Namespace, Resource, marshal
